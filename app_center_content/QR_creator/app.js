58b4cd58a010ec5f2b1f197fe4e63c4c30cd3559
Baseus827 <115733785+Baseus827@users.noreply.github.com>
2023-04-20 06:22:51 +0300

    fix

--- app_center_content/QR_creator/app.js
+++ app_center_content/QR_creator/app.js
@@ -1,58 +1,29 @@
-// app.js
-
-// Input elementine erişmek
 var input = document.querySelector(".search-box");
-
-// Generate button'a erişmek
 var generateBtn = document.querySelector(".create-button");
-
-// Download button'a erişmek
 var downloadBtn = document.querySelector(".download-button");
-
-// QR kodun oluşturulacağı elementi seçmek
 var qrCode = document.querySelector("#qr-code");
-
-// QR kodu oluşturma ve gösterme işlevi
 function showQRCode() {
-  // Eski QR kodu varsa, önce sil
-  qrCode.innerHTML = "";
-
-  // Yeni QR kodunu oluştur
-  var qr = new QRCode(qrCode, {
-    text: input.value,
-    width: 256,
-    height: 256,
-    colorDark: "#000000",
-    colorLight: "#ffffff",
-    correctLevel: QRCode.CorrectLevel.H,
-  });
+qrCode.innerHTML = "";
+var qr = new QRCode(qrCode, {
+text: input.value,
+width: 256,
+height: 256,
+colorDark: "#000000",
+colorLight: "#ffffff",
+correctLevel: QRCode.CorrectLevel.H,
+});
 }
-
-// QR kodu oluşturmak için generate butonuna tıklanması
 generateBtn.addEventListener("click", showQRCode);
-
-// QR kodunu indirmek için download butonuna tıklanması
 downloadBtn.addEventListener("click", downloadQRCode);
-
-// QR kodunu indirme işlevi
 function downloadQRCode() {
-  // QR kodunu içeren canvas elementini seç
-  var canvas = qrCode.querySelector("canvas");
-
-  // QR kodunu indirme için bir link oluştur
-  var link = document.createElement("a");
-  link.download = "qrcode.png";
-
-  // Canvas elementini image/png formatına dönüştürmek için URL oluştur
-  link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
-
-  // Link'e tıklanmasını simüle etmek için bir mouse eventi oluştur
-  var clickEvent = new MouseEvent("click", {
-    view: window,
-    bubbles: true,
-    cancelable: true,
-  });
-
-  // Link'e tıklanması için event'i tetikle
-  link.dispatchEvent(clickEvent);
+var canvas = qrCode.querySelector("canvas");
+var link = document.createElement("a");
+link.download = "qrcode.png";
+link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+var clickEvent = new MouseEvent("click", {
+view: window,
+bubbles: true,
+cancelable: true,
+});
+link.dispatchEvent(clickEvent);
 }
